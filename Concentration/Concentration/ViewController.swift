2868957f44279831f529e59e7e7f87028ec5b70f
Rafael Nunes Rios <rafaelrafssk@hotmail.com>
2020-10-08 12:52:23 -0300

    first lecture

---
+++ Concentration/Concentration/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by Rafael Nunes Rios on 10/8/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var flipCount  = 0 {
+        didSet {
+            flipCountLabel.text = "Flips: \(flipCount)"
+        }
+    }
+    
+    @IBOutlet weak var flipCountLabel: UILabel!
+    
+    @IBAction func touchCard(_ sender: UIButton) {
+        flipCount += 1
+    }
+
+    
+    func flipCard(withEmoji emoji: String, on button: UIButton) {
+        if button.currentTitle == emoji {
+            button.setTitle("", for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        }else {
+            button.setTitle(emoji, for: UIControl.State.normal)
+            button.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
+        }
+    }
+
+
+}
+
